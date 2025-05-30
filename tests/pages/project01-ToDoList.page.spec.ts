508693870c1a8fefd42fade9250d852c42fc7e0a
Eddie C. <edimc2000@gmail.com>
2025-05-29 23:32:31 -0500

    TC01 and TC02

---
+++ tests/pages/project01-ToDoList.page.spec.ts
@@ -0,0 +1,70 @@
+import { Page, expect, test } from "@playwright/test"
+
+export default class ToDoListPage {
+    locators: any
+    page: any
+
+    constructor(page: Page) {
+        this.page = page
+        this.locators = {
+            getTextPageHeader: page.locator('h1.is-size-3'),
+            getTextModalTitle: page.locator('.panel-heading.has-text-white'),
+            getButtonAdd: page.locator('#add-btn'),
+            getFieldAddToDo: page.locator('#input-add'),
+            getFieldSearch: page.locator('#search'),
+            getTextMessageContainer: page.locator('.ml-1.has-text-danger'),
+            getPanelToDosContainer: page.locator('#panel'),
+
+            getPanelToDosList: page.locator('.panel-block.todo-item div'),
+            getPanelToDosListMarker: page.locator('.panel-block.todo-item div span:first-child'),
+            getPanelToDosListLastItem: page.locator('div.panel-block div span:last-child').last(),
+
+            getPanelToDoMarkNotDone: page.locator('.panel-icon:nth-child(odd)'),
+            getPanelRemoveTaskLastRow: page.locator('.panel-block.todo-item .destroy').last(),
+
+            getPanelToDoDelete: page.locator('.destroy'),
+            getRemoveCompleted: page.locator('#clear'),
+            getErrorContainer: page.locator('.notification.is-danger')
+
+        }
+    }
+
+    /**
+     * @param task is the sting value of the task 
+     */
+    async createTask(task) {
+        await this.locators.getFieldAddToDo.clear()
+        await this.locators.getFieldAddToDo.fill(task)
+        await this.locators.getButtonAdd.click()
+    }
+
+    /**
+     * @param  task - last task on the list to verify
+     */
+    async verifyTaskOnList(task: string) {
+        expect(await this.locators.getPanelToDosListLastItem).toHaveText(task)
+    }
+    
+    /**
+    * @param {number} expectedNumberOfTasks  
+    */
+    async countTask(expectedNumberOfTasks: number) {
+        // console.log('nO OF TASKSS:', (await this.locators.getPanelToDosListMarker.count()))
+        // console.log('EXPECTED:', expectedNumberOfTasks)
+        expect(await this.locators.getPanelToDosListLastItem.count()).toBe(expectedNumberOfTasks)
+    }
+
+    async markComplete (row: number) {
+         await this.locators.getPanelToDosListLastItem.click()
+         expect(await this.locators.getPanelToDosListMarker.last()).toHaveClass(/has-text-success/)
+    }
+
+    async removeLastRowTask () {
+          await this.locators.getPanelRemoveTaskLastRow.click()
+     }
+
+     async verifyTaskPanelIsEmpty () {
+        expect(await this.locators.getTextMessageContainer).toHaveText('No tasks found!')
+     }
+
+}
--- tests/specs/project01.spec.ts
+++ tests/specs/project01.spec.ts
@@ -1,1 +1,77 @@
-import {expect, test} from '@playwright/test'
+import { expect, test } from '@playwright/test'
+import fs from 'fs';
+import path from 'path';
+import { parse } from 'csv-parse/sync';
+
+import ToDoListPage from '../pages/project01-ToDoList.page.spec'
+
+
+
+test.describe('TG Todo List', () => {
+    let locators
+    let methods
+
+    // covert sampleToDoData csv to JS Object
+    const sampleToDoData = parse(fs.readFileSync(path.join(__dirname, '..', 'data/project01toDoListPageSampleData.csv')), {
+        columns: true,
+        skip_empty_lines: true
+    });
+
+    console.log(sampleToDoData)
+    console.log(sampleToDoData[0].toDo)
+
+
+    test.beforeEach(async ({ page }) => {
+
+        methods = new ToDoListPage(page)
+        locators = methods.locators
+        await page.setViewportSize({ width: 1440, height: 1440 });
+        await page.goto('https://www.techglobal-training.com/frontend/todo-list')
+    })
+
+    /*
+    [TC01] Todo-App Modal Verification
+    * 1. Navigate to https://techglobal-training.com/frontend/todo-list.
+    * 2. Confirm that the todo-app modal is visible with the title “My Tasks.”
+    * 3. Validate that the New todo input field is enabled for text entry.
+    * 4. Validate ADD button is enabled.
+    * 5. Validate Search field is enabled.
+    * 6. Validate that the task list is empty, displaying the message “No tasks found!”
+    */
+    test('[TC01] Todo-App Modal Verification', async ({ page }) => {
+        expect(await locators.getTextModalTitle).toHaveText('My Tasks')
+        expect(await locators.getFieldAddToDo).toBeEnabled()
+        expect(await locators.getButtonAdd).toBeEnabled()
+        expect(await locators.getFieldSearch).toBeEnabled()
+        expect(await locators.getPanelToDosContainer).toBeHidden()
+        await page.waitForTimeout(100)
+    })
+
+    /* 
+    [TC02]  Single Task Addition and Removal
+    * 1. Navigate to https://techglobal-training.com/frontend/todo-list
+    * 2. Enter a new task in the todo input field and add it to the list.
+    * 3. Validate that the new task appears in the task list.
+    * 4. Validate that the number of tasks in the list is exactly one.
+    * 5. Mark the task as completed by clicking on it.
+    * 6. Validate item is marked as completed.
+    * 7. Click on the button to remove the item you have added.
+    * 8. Remove the completed task by clicking the designated removal button.
+    9. Validate that the task list is empty, displaying the message “No tasks found!”.
+    */
+    test.only('[TC02] Single Task Addition and Removal', async ({ page }) => {
+        let taskArr = sampleToDoData.slice(0,2)
+
+        for (const toDo of taskArr) {
+            await methods.createTask(toDo.toDo)
+            await methods.verifyTaskOnList(toDo.toDo)
+
+            await methods.markComplete(taskArr.length)
+            await methods.countTask(taskArr.length)
+            // await methods.removeLastRowTask()
+            // await methods.verifyTaskPanelIsEmpty()
+        }
+        await methods.countTask(taskArr.length)
+    })
+
+})
